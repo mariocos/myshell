3858aa1a4ccb2a90b05a0990e90d7dc115ec9bad
Mario Costa De Souza <mariocos@c2r2s8.42lisboa.com>
2024-12-11 15:11:54 +0000

    fixed path

--- srcs/pipex/child.c
+++ srcs/pipex/child.c
@@ -3,14 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   child.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hugo-mar <hugo-mar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mariocos <mariocos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 16:47:59 by mariocos          #+#    #+#             */
-/*   Updated: 2024/12/11 14:21:18 by hugo-mar         ###   ########.fr       */
+/*   Updated: 2024/12/11 15:11:43 by mariocos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	hold;
+
+	hold = c;
+	while (*s)
+	{
+		if (*s == hold)
+			return ((char *)s);
+		s++;
+	}
+	if (hold == '\0')
+		return ((char *)s);
+	return (0);
+}
 
 void	child_process_new(t_pipex	*p)
 {
@@ -29,7 +45,10 @@ void	child_process_new(t_pipex	*p)
 		exec_if_builtin(p);
 	else
 	{
-		path = path_search(p->cmd[0], mini_call()->env);
+		if (!ft_strchr(p->cmd[0], '/'))
+			path = path_search(p->cmd[0], mini_call()->env);
+		else
+			path = p->cmd[0];
 		envp = env_to_double_chr_ptr(mini_call()->env);
 		if (execve(path, p->cmd, envp) == -1)
 			printf("EXECVE - NOTHING HAPPENED\n");
