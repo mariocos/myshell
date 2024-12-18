50a0e728275afd622058a1ee6d0e809585bf52ad
hugo-mar <hugo.alberto.dias@gmail.com>
2024-12-18 23:01:43 +0000

    Refactorated functions to meet norminette requierements

--- srcs/pipex/redir/prep_input_redir.c
+++ srcs/pipex/redir/prep_input_redir.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   prep_input_redir.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mariocos <mariocos@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hugo-mar <hugo-mar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 14:43:47 by mariocos          #+#    #+#             */
-/*   Updated: 2024/12/17 22:14:56 by mariocos         ###   ########.fr       */
+/*   Updated: 2024/12/18 22:58:54 by hugo-mar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,21 @@
 Handles HEREDOC input, expands variables, writes to a pipe,
 and exits on completion
 */
-void	read_into_pipe(char *eof, t_pipex *p)
+static void	process_input_and_write(char *help, int doc_pipe_fd)
 {
 	char	*str;
+
+	if (*help)
+		str = here_doc_expand(help);
+	else
+		str = help;
+	help = ft_strjoin(str, "\n");
+	write(doc_pipe_fd, help, ft_strlen(help));
+	help_free(help, str);
+}
+
+void	read_into_pipe(char *eof, t_pipex *p)
+{
 	char	*help;
 
 	if_close(p->doc_pipe[0]);
@@ -29,24 +41,26 @@ void	read_into_pipe(char *eof, t_pipex *p)
 		{
 			close(p->doc_pipe[1]);
 			print_ctrl_d_msg(eof);
-			free (help);
-			exit (144);
+			free(help);
+			exit(144);
 		}
 		if (!ft_strcmp(help, eof))
 		{
 			free(help);
 			break ;
 		}
-		if (*help)
-			str = here_doc_expand(help);
-		else
-			str = help;
-		help = ft_strjoin(str, "\n");
-		write(p->doc_pipe[1], help, ft_strlen(help));
-		help_free(help, str);
+		process_input_and_write(help, p->doc_pipe[1]);
 	}
 	close(p->doc_pipe[1]);
 	exit(0);
+}
+
+static void	wait_and_restore_signal_handlers(int pid, t_pipex *p)
+{
+	setup_signal_handlers_heredoc();
+	if_close(p->doc_pipe[1]);
+	ft_waitpid(pid);
+	setup_signal_handlers();
 }
 
 /*
@@ -74,10 +88,7 @@ static int	do_here_doc(char *str, t_pipex *p)
 		setup_child_process_signal_handlers();
 		read_into_pipe(str, p);
 	}
-	setup_signal_handlers_heredoc();
-	if_close(p->doc_pipe[1]);
-	ft_waitpid(pid);
-	setup_signal_handlers();
+	wait_and_restore_signal_handlers(pid, p);
 	if (p->has_doc && mini_call()->exit_status != 0)
 		return (-1);
 	return (1);
@@ -106,6 +117,21 @@ void	if_close(int fd)
 		close (fd);
 }
 
+static int	handle_redirection_line(t_pipex *p, int i, int *error_flag)
+{
+	if (!ft_strncmp(p->red_in[i], "STD:", 4) && *error_flag == -1)
+		prep_helper_file_opener(p, error_flag, i);
+	else if (!ft_strncmp(p->red_in[i], "APP:", 4))
+	{
+		if (do_here_doc(p->red_in[i] + 4, p) < 0)
+		{
+			p->bad_command = true;
+			return (-1);
+		}
+	}
+	return (0);
+}
+
 /*
 Processes input redirections and HEREDOC for all pipeline commands
 */
@@ -122,16 +148,8 @@ int	prep_input_redir(t_pipex *p)
 		i = -1;
 		while (p->red_in && p->red_in[++i])
 		{
-			if (!ft_strncmp(p->red_in[i], "STD:", 4) && error_flag == -1)
-				prep_helper_file_opener(p, &error_flag, i);
-			else if (!ft_strncmp(p->red_in[i], "APP:", 4))
-			{
-				if (do_here_doc(p->red_in[i] + 4, p) < 0)
-				{
-					p->bad_command = true;
-					break ;
-				}
-			}
+			if (handle_redirection_line(p, i, &error_flag) < 0)
+				break ;
 		}
 		p = p->next;
 	}
