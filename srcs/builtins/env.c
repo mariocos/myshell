254c59c3313fce13007d2861d8346fd8f224a1af
hugo-mar <hugo.alberto.dias@gmail.com>
2025-01-08 17:00:42 +0000

    env works again with path unset

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,22 +6,11 @@
 /*   By: hugo-mar <hugo-mar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/31 18:01:56 by hugo-mar          #+#    #+#             */
-/*   Updated: 2024/12/31 18:06:23 by hugo-mar         ###   ########.fr       */
+/*   Updated: 2025/01/08 16:59:27 by hugo-mar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
-static bool	has_path_variable(t_env *env)
-{
-	while (env)
-	{
-		if (!ft_strncmp(env->var_name, "PATH", 5))
-			return (true);
-		env = env->next;
-	}
-	return (false);
-}
 
 static void	handle_invalid_arg(char **args, int fd)
 {
@@ -44,12 +33,6 @@ void	env(t_env *env, char **args, int fd)
 	if (!env)
 	{
 		mini_call()->exit_status = 1;
-		return ;
-	}
-	if (!has_path_variable(env))
-	{
-		write(2, "minishell: env: No such file or directory\n", 42);
-		mini_call()->exit_status = 127;
 		return ;
 	}
 	if (args[1])
